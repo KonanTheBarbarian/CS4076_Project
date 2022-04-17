308405f7bfd01ba82fd2c263779b7a233c4698a2
Konan H <91267422+KonanTheBarbarian@users.noreply.github.com>
2022-04-17 12:14:55 +0100

    Create constants.h

---
+++ constants.h
@@ -0,0 +1,21 @@
+#ifndef CONSTANTS_H
+#define CONSTANTS_H
+
+#include <string>
+using std::string;
+
+// Namespace containing string constants.
+namespace Constants{
+    const string SOLAR_SYSTEM_PIC = ":/img/img/solar_system.png";
+    const string MERCURY_PIC = ":/img/img/mercury.png";
+    const string VENUS_PIC = ":/img/img/venus.png";
+    const string EARTH_PIC = ":/img/img/earth.png";
+    const string MARS_PIC = ":/img/img/mars.png";
+    const string JUPITER_PIC = ":/img/img/jupiter.png";
+    const string SATURN_PIC = ":/img/img/saturn.png";
+    const string URANUS_PIC = ":/img/img/uranus.png";
+    const string NEPTUNE_PIC = ":/img/img/neptune.png";
+    const string PLUTO_PIC = ":/img/img/pluto.png";
+}
+
+#endif // CONSTANTS_H
